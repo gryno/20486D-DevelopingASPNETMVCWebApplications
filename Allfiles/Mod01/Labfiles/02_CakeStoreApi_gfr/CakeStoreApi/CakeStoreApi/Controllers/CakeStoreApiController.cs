5600bc095a5bda35eb7ca1371ddc6aa4e62e1b43
Gary Rynearson <rynocode@gmail.com>
2020-07-16 22:13:44 -0400

    Excersize 2 Finished

---
+++ Allfiles/Mod01/Labfiles/02_CakeStoreApi_gfr/CakeStoreApi/CakeStoreApi/Controllers/CakeStoreApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CakeStoreApi.Models;
+
+namespace CakeStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CakeStoreApiController : ControllerBase
+    {
+        private IData _data;
+
+        public CakeStoreApiController(IData data)
+        {
+            _data = data;
+        }
+
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+
+        [HttpGet("/api/CakeStore/{id}", Name = "GetCake")]
+        public ActionResult<CakeStore> GetById(int? id)
+        {
+            var item = _data.GetCakeById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
--- Allfiles/Mod01/Labfiles/02_CakeStoreApi_gfr/CakeStoreApi/CakeStoreApi/Startup.cs
+++ Allfiles/Mod01/Labfiles/02_CakeStoreApi_gfr/CakeStoreApi/CakeStoreApi/Startup.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CakeStoreApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CakeStoreApi
 {
@@ -23,13 +23,14 @@ namespace CakeStoreApi
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IData, Data>();
-            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddControllers();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,7 +38,11 @@ namespace CakeStoreApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
