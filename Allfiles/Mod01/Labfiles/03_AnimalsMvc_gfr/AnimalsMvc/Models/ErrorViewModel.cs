2dba994be7a31434a56aba4d365f2e70d807000e
Gary Rynearson <rynocode@gmail.com>
2020-07-17 09:26:36 -0400

    last update for branch

---
+++ Allfiles/Mod01/Labfiles/03_AnimalsMvc_gfr/AnimalsMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnimalsMvc_Gfr.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ Allfiles/Mod01/Labfiles/03_AnimalsMvc_gfr/AnimalsMvc/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalsMvc_Gfr.Models
+{
+    public class IndexViewModel
+    {
+        public List<Animal> Animals { get; set; }
+    }
+}
